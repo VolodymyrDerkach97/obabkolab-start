439161489498bcb0488486111200d8e267814ad9
Volodymyr Derkach <113111513+VolodymyrDerkach97@users.noreply.github.com>
2023-06-26 23:52:22 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { AppA, ImgPes, Clen } from "./app.styled";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppA>
+      <Clen>Start</Clen>
+      <ImgPes src="https://picsum.photos/id/237/600" alt="" />
+    </AppA>
   );
 }
 
---
+++ src/app.styled.js
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+export const AppA = styled.div`
+  position: relative;
+`;
+export const ImgPes = styled.img`
+  position: absolute;
+  z-index: 999;
+`;
+export const Clen = styled.div`
+  position: absolute;
+  font-size: 80px;
+  left: 277px;
+  top: 127px;
+  color: red;
+  z-index: 9999;
+`;
